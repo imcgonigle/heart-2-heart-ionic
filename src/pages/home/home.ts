ec066cec441446c1d9367692071502f49ef384e8
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-26 23:59:50 -0600

    add videos and pictures to emotions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,15 +12,50 @@ export class HomePage {
   emotions: any = {
     happy: {
       title: 'Happy',
-      greeting: 'Check out some of the videos and pictures on this page. They\'re sure to brighten your day.'
+      greeting: 'Check out some of the videos and pictures on this page. They\'re sure to brighten your day.',
+      videos: [
+        {title: 'Dog Playing'},
+        {title: 'Baby Laughing'},
+        {title: 'Roller Coaster'},
+        {title: 'Christmas Party'}
+      ],
+      pictures: [
+        {title: 'The Beach'},
+        {title: 'Family'},
+        {title: 'Dancing'}
+      ]
     },
     relaxing: {
       title: 'Relaxing',
-      greeting: 'If you\'re trying to relax then you\'ve come to the right place.'
+      greeting: 'If you\'re trying to relax then you\'ve come to the right place.',
+      videos: [
+        {title: 'Floating on Clouds'},
+        {title: 'The Waves Crashing'},
+        {title: 'A Pond'},
+        {title: 'Floating Through Space'},
+        {title: 'Bob Ross'}
+      ],
+      pictures: [
+        {title: 'Dogs Sleeping'},
+        {title: 'Mattress Sale'},
+        {title: 'Boat on the Ocean'}
+      ]
     },
     satisfying: {
       title: 'Satisfying',
-      greeting: 'You\'re sure to be satisfied after taking a look at the stuff we have for you here.'
+      greeting: 'You\'re sure to be satisfied after taking a look at the stuff we have for you here.',
+      videos: [
+        {title: 'Cutting the Cake'},
+        {title: 'Skateboard Tricks'},
+        {title: 'Sports Highlights'},
+        {title: 'Magic Tricks'}
+      ],
+      pictures: [
+        {title: 'Space Ship'},
+        {title: 'Cakes'},
+        {title: 'Art'},
+        {title: 'Parachuting'}
+      ]
     }
   }
 
