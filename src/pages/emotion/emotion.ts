5ccf2d224b1d4843a4e44685513e2af1f53e885c
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-26 22:35:07 -0600

    add a segment bar

--- src/pages/emotion/emotion.ts
+++ src/pages/emotion/emotion.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EmotionPage {
   private title: string;
+  private mediaType: string = "videos";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.title = navParams.get('title');
