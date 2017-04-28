8322850c899dfcac889ef02ad85411a74bd1b013
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-28 01:44:33 -0600

    add dummy pictures

--- src/providers/emotion.provider.ts
+++ src/providers/emotion.provider.ts
@@ -16,10 +16,10 @@ export class EmotionProvider {
       greeting: 'Check out some of the videos and pictures on this page. They\'re sure to brighten your day.',
       imageUrl:'assets/images/emojis-128/cowboy.png',
       videos: [
-        {title: 'Dog Playing'},
-        {title: 'Baby Laughing'},
-        {title: 'Roller Coaster'},
-        {title: 'Christmas Party'}
+        {title: 'Dog Playing', thumbnailUrl: 'assets/images/videos/happy/1.jpeg'},
+        {title: 'Baby Laughing', thumbnailUrl: 'assets/images/videos/happy/2.jpeg'},
+        {title: 'Roller Coaster', thumbnailUrl: 'assets/images/videos/happy/3.jpeg'},
+        {title: 'Christmas Party', thumbnailUrl: 'assets/images/videos/happy/4.jpeg'}
       ],
       pictures: [
         {title: 'The Beach', imageUrl: 'assets/images/emojis-128/sour.png'},
@@ -32,11 +32,11 @@ export class EmotionProvider {
       greeting: 'If you\'re trying to relax then you\'ve come to the right place.',
       imageUrl: 'assets/images/emojis-128/robot.png',
       videos: [
-        {title: 'Floating on Clouds'},
-        {title: 'The Waves Crashing'},
-        {title: 'A Pond'},
-        {title: 'Floating Through Space'},
-        {title: 'Bob Ross'}
+        {title: 'Floating on Clouds', thumbnailUrl: 'assets/images/videos/relaxing/1.jpeg'},
+        {title: 'The Waves Crashing', thumbnailUrl: 'assets/images/videos/relaxing/2.jpeg'},
+        {title: 'A Pond', thumbnailUrl: 'assets/images/videos/relaxing/3.jpeg'},
+        {title: 'Floating Through Space', thumbnailUrl: 'assets/images/videos/relaxing/4.jpeg'},
+        {title: 'Bob Ross', thumbnailUrl: 'assets/images/videos/relaxing/5.jpeg'}
       ],
       pictures: [
         {title: 'Dogs Sleeping', imageUrl: 'assets/images/emojis-128/drooling.png'},
@@ -49,10 +49,10 @@ export class EmotionProvider {
       greeting: 'You\'re sure to be satisfied after taking a look at the stuff we have for you here.',
       imageUrl: 'assets/images/emojis-128/happy-hands.png',
       videos: [
-        {title: 'Cutting the Cake'},
-        {title: 'Skateboard Tricks'},
-        {title: 'Sports Highlights'},
-        {title: 'Magic Tricks'}
+        {title: 'Cutting the Cake', thumbnailUrl: 'assets/images/videos/satisfying/1.jpeg'},
+        {title: 'Skateboard Tricks', thumbnailUrl: 'assets/images/videos/satisfying/2.jpeg'},
+        {title: 'Sports Highlights', thumbnailUrl: 'assets/images/videos/satisfying/3.jpeg'},
+        {title: 'Magic Tricks', thumbnailUrl: 'assets/images/videos/satisfying/4.jpeg'}
       ],
       pictures: [
         {title: 'Space Ship', imageUrl: 'assets/images/emojis-128/laughing-tears-tilted.png'},
