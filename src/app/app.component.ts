9d0230510edf35f04da9e876e7c5e3b8081d0b45
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-28 14:31:46 -0600

    remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { EmotionPage } from '../pages/emotion/emotion';
 
 @Component({
   templateUrl: 'app.html'
