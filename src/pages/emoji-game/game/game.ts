d1239ebf8df92267b3c229d7c3e45c93eb46b0e0
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-28 14:55:05 -0600

    add array of emoji urls

--- src/pages/emoji-game/game/game.ts
+++ src/pages/emoji-game/game/game.ts
@@ -14,6 +14,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GamePage {
 
+  emojis: any[] = [
+    'assets/images/emoji-128/blow-kiss.png',
+    'assets/images/emoji-128/blow.png',
+    'assets/images/emoji-128/blushing.png',
+    'assets/images/emoji-128/cat-angry.png',
+    'assets/images/emoji-128/cat-happy-tears.png',
+    'assets/images/emoji-128/cat-happy-eyes.png',
+    'assets/images/emoji-128/cat-shocked.png',
+    'assets/images/emoji-128/cat-smirk.png',
+    'assets/images/emoji-128/cat-tear.png',
+    'assets/images/emoji-128/cat-whistle.png',
+    'assets/images/emoji-128/clown.png',
+    'assets/images/emoji-128/cool.png',
+    'assets/images/emoji-128/cowboy.png',
+    'assets/images/emoji-128/crying-tears.png',
+    'assets/images/emoji-128/crying.png',
+    'assets/images/emoji-128/devil.png',
+    'assets/images/emoji-128/displeased.png',
+    'assets/images/emoji-128/down.png',
+    'assets/images/emoji-128/drooling.png',
+    'assets/images/emoji-128/excited.png',
+    'assets/images/emoji-128/eyes-closed.png',
+    'assets/images/emoji-128/frown-long.png',
+    'assets/images/emoji-128/frown.png',
+    'assets/images/emoji-128/happy-cat.png',
+    'assets/images/emoji-128/happy-hands.png',
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
